85c4f6ef1766f5d03ba7aacbf48271cd2c528e08
Michael <mdlawrence@utexas.edu>
2016-08-09 16:23:28 -0500

    cleaned cpu_usage by creating two functions
    need to implement test cases if possible

--- covert.c
+++ covert.c
@@ -6,10 +6,11 @@
 int most_cpu_usage(){
     long double dump[10];
     int cpu_number = 0;
-    int i = 0;//iterator
     FILE *cpu_folder = fopen("/proc/stat", "r");
     fscanf(cpu_folder, "%*s %Lf %Lf %Lf %Lf %Lf %Lf %Lf %Lf %Lf %Lf",&dump[0],&dump[1],&dump[2],&dump[3],&dump[4],&dump[5],&dump[6],&dump[7],&dump[8],&dump[9]);//dumps total usage    
-    
+    for(int i = 0; i<4; i++){
+        fscanf(cpu_folder, "%*s %Lf %Lf %Lf %Lf")      
+    }
 }
 
 
--- test.h
+++ test.h
@@ -2,5 +2,5 @@ int main(int argc, char *argv[]);
 int load_count(void);
 int which_cpu(void);
 void cpu_usage(int cpu_number);
-
-
+long double cpu_math(long double a[4], long double b[4]);
+void cpu_access(FILE *cpu_folder, long double *a);
