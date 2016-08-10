3add264813a06954e88b5de409d6ea68fe27c643
Michael <mdlawrence@utexas.edu>
2016-08-10 18:51:26 -0500

    created the load_lag function
    This function is meant to decrease loads
    for the receiver
    
    Now I'll try to send a bit pattern through the
    .txt file

---
+++ slow_this.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void main(){
+   /* char a;
+    char b =0;
+    int i;
+    time_t start, diff;
+    for(int again = 0; again<20000; again++){
+        start = clock();
+        for(i = 0; diff<)
+    }*/
+    int a;
+    int b = 0;
+    while(1)a = b;
+}
